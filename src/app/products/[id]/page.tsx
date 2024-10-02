b9acfc4891ded6f027a791ea35059763d5bb75b8
nurlancreus <nurlancreus007@gmail.com>
2024-10-02 13:23:23 +0400

    .

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -2,7 +2,7 @@ import { ProductType } from "@/types";
 import { fetchData } from "@/utils/helpers";
 import ProductInfo from "./_components/page/product-info";
 
-export default async function Product({ params }: { params: { id: number } }) {
+export default async function Product({ params }: { params: { id: string } }) {
   console.log(params.id);
 
   const product = await fetchData<ProductType>(`/products/${params.id}`);
